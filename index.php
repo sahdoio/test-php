fa66269bf2ad0e769233064bc669f4627926c6f8
Lucas Sahdo <lsahdo@insiderealestate.com>
2023-07-06 14:52:04 -0300

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="scripts.js">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.php">
+    <title>Document</title>
+</head>
+<body>
+
+   <?php
+        echo "teste";
+   ?>
+
+</body>
+</html>
